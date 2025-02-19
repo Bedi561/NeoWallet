b6739359c9c359eff5e68f64e0c63aa850c437b9
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2025-02-19 12:41:53 +0530

    new4

--- client/src/app/transaction-details/[id]/page.tsx
+++ client/src/app/transaction-details/[id]/page.tsx
@@ -1,7 +1,12 @@
 import TransactionDetailsContent from "@/components/ui/transaction-details-content"
 
-export default function TransactionDetailsPage({ params }: { params: { id: string } }) {
-  return <TransactionDetailsContent transactionId={params.id} />
+interface PageProps {
+  params: {
+    id: string
+  }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-
+export default function TransactionDetailsPage({ params }: PageProps) {
+  return <TransactionDetailsContent transactionId={params.id} />
+}
